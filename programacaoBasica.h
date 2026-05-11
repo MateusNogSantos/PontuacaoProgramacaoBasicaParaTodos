7ccaae0e50419d7c27b3ab87ba2671c3541c84ed
mateus <nmateusjogos@gmail.com>
2026-05-11 07:58:05 -0300

    retirada de comentários

--- programacaoBasica.h
+++ programacaoBasica.h
@@ -5,11 +5,10 @@
 #include <utility>
 #include <queue>
 #include <stdexcept>
-#include <algorithm> // Necessário para o std::max
+#include <algorithm> 
 
 using namespace std;
 
-// Forward Declaration para a classe Evento poder acessar membros de Grupo
 class Evento;
 
 class Grupo {
@@ -27,10 +26,8 @@ private:
     void ajudou();
 
 public:
-    // Construtor
     Grupo();
 
-    // Getters
     int getPontuacao() { return pontuacao; }
     int getExerciciosFaceis() { return exerciciosFaceis.size(); }
     int getExerciciosMedios() { return exerciciosMedios.size(); }
@@ -44,21 +41,18 @@ private:
     vector<Grupo> equipe;
 
 public:
-    // Construtor
+
     Evento(int numeroEquipes);
 
-    // Métodos de Logica
     void concluiuExercicioMedio(int indiceEquipe);
     void concluiuExercicioFacil(int indiceEquipe);
     void ajudou(int indiceEquipe);
 
-    // Retorna a fila de prioridade para a tabela final
     priority_queue<pair<int,int>, vector<pair<int,int>>, less<pair<int,int>>> mostrarTabela();
 
-    // Getters de estado
     int getAjudas(int i);
     int getExerciciosFaceis(int i);
     int getExerciciosMedios(int i);
 };
 
-#endif // EVENTO_H
+#endif
